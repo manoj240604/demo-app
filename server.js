f025735d3eee414ec30420e3a549cb238ac2b2da
manoj240604 <manojmukodihalli@gmail.com>
2026-07-16 07:00:10 +0530

    Initial Demo App

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Demo App");
+});
+
+app.listen(3000, () => {
+    console.log("Demo App is running on port 3000");
+});
